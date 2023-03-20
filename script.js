0a53989a7ede19a760368e1418a5e8c7537be711
Igor Stepanov <stipyk1309@gmail.com>
2023-03-20 09:43:51 +0300

    adaptive, font, logic

--- script.js
+++ script.js
@@ -9,12 +9,24 @@ const slider3Value = document.querySelector(".month");
 slider1.addEventListener("input", function () {
   slider1Value.textContent = `${slider1.value} ₽`;
 });
+
 slider2.addEventListener("input", function () {
   slider2Value.textContent = `${slider2.value} %`;
-  contributionRubles.textContent = `${
+  contributionRubles.textContent = `${Math.trunc(
     (parseInt(slider1.value) * parseInt(slider2.value)) / 100
-  } ₽`;
+  )} ₽`;
 });
+
 slider3.addEventListener("input", function () {
   slider3Value.textContent = `${slider3.value} месяцев`;
 });
+
+//inputs progress
+for (let e of document.querySelectorAll(
+  'input[type="range"].slider-progress'
+)) {
+  e.style.setProperty("--value", e.value);
+  e.style.setProperty("--min", e.min == "" ? "0" : e.min);
+  e.style.setProperty("--max", e.max == "" ? "100" : e.max);
+  e.addEventListener("input", () => e.style.setProperty("--value", e.value));
+}
