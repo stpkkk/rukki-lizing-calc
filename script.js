e5588ac99da077b5fdbc7042af652318fc64fb97
Igor Stepanov <stipyk1309@gmail.com>
2023-03-20 18:24:36 +0300

    monthlyPayment add logic

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const slider1 = document.querySelector("#slider1");
-const slider1Value = document.querySelector(".price-value");
+const priceValue = document.querySelector(".price-value");
+
 const slider2 = document.querySelector("#slider2");
-const slider2Value = document.querySelector(".precent");
-const contributionRubles = document.querySelector(".prise");
+const percentValue = document.querySelector(".percent");
+const contributionRubles = document.querySelector(".paymentPrice");
+
 const slider3 = document.querySelector("#slider3");
 const slider3Value = document.querySelector(".month");
 
+const contractSum = document.querySelector(".sum-contract");
+const monthPayment = document.querySelector(".calculator__price");
+
+monthPayment.textContent = "0 ₽";
+priceValue.textContent = "0 ₽";
+
+function calculateMonthlyPayment() {
+  const monthlyPayment =
+    (parseInt(slider1.value) - parseInt(contributionRubles.textContent)) /
+    parseInt(slider3.value);
+  monthlyPayment < 0
+    ? (monthPayment.textContent = "0 ₽")
+    : (monthPayment.textContent = `${Math.trunc(monthlyPayment)} ₽`);
+}
+
 slider1.addEventListener("input", function () {
-  slider1Value.textContent = `${slider1.value} ₽`;
+  priceValue.textContent = `${slider1.value} ₽`;
+  contractSum.textContent = `${slider1.value} ₽`;
+  calculateMonthlyPayment();
 });
 
 slider2.addEventListener("input", function () {
-  slider2Value.textContent = `${slider2.value} %`;
+  percentValue.textContent = `${slider2.value} %`;
   contributionRubles.textContent = `${Math.trunc(
     (parseInt(slider1.value) * parseInt(slider2.value)) / 100
   )} ₽`;
+  calculateMonthlyPayment();
 });
 
 slider3.addEventListener("input", function () {
   slider3Value.textContent = `${slider3.value} месяцев`;
+  calculateMonthlyPayment();
 });
+
+// calculate initial monthly payment
+calculateMonthlyPayment();
 
 //inputs progress
 for (let e of document.querySelectorAll(
